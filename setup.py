52f08d9e2cfc62ba2c3fa1537ba0a272499a6896
Jonny Graves <jgraves@dyn.com>
2019-06-29 13:23:55 -0400

    Version bump and concourse test

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jockbot_nhl',
-      version='0.0.10',
+      version='0.0.11',
       description='Retrieve info from NHL API',
       url='http://github.com/jalgraves/jockbot_nhl',
       author='Jonny Graves',
